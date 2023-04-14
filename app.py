a8d1b4371055aa732d2d036cb2c34b2a2c843bfe
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2023-04-14 08:46:06 -0300

    3D graphics test

--- app.py
+++ app.py
@@ -8,15 +8,24 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         eixox1 = request.form.get("eixox1")
-        eixox2 = request.form.get("eixox2")
         eixoy1 = request.form.get("eixoy1")
+        eixoz1 = request.form.get("eixoz1")
+
+        eixox2 = request.form.get("eixox2")
         eixoy2 = request.form.get("eixoy2")
+        eixoz2 = request.form.get("eixoz2")
 
-        xaxis = np.array([eixox1, eixox2])
-        yaxis = np.array([eixoy1 , eixoy2])
+        eixox3 = request.form.get("eixox3")
+        eixoy3 = request.form.get("eixoy3")
+        eixoz3 = request.form.get("eixoz3")
 
-        plt.plot(xaxis, yaxis)
+        x= np.array([eixox1, eixox2 , eixoz1 ],dtype=float)
+        y= np.array([eixoy1 , eixoy2 , eixoz2],dtype=float)
+        z= np.array([eixox3, eixoy3, eixoz3],dtype=float)
+        ax = plt.figure().add_subplot(projection='3d')
+        ax.plot(x,y,z)
         plt.savefig('static\images\graph.png')
+        plt.show()
     return render_template("index.html")
 
 @app.route("/graph")
