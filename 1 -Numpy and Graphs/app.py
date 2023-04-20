90eb87249f77785f824b81ea5d0f1b42d1b0ac14
MarkVN2 <markosvinicius.nunes@gmail.com>
2023-04-20 08:55:55 -0300

    Reorganization of 'Numpy and Graphs'
    Moved it to a specific directory.

--- 1 -Numpy and Graphs/app.py
+++ 1 -Numpy and Graphs/app.py
@@ -4,9 +4,12 @@ import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
+#TODO make the README.md for this exercise for better explanation
+
 @app.route("/", methods =["GET", "POST"])
 def index():
     if request.method == "POST":
+        
         eixox1 = request.form.get("eixox1")
         eixoy1 = request.form.get("eixoy1")
         eixoz1 = request.form.get("eixoz1")
@@ -22,15 +25,18 @@ def index():
         x= np.array([eixox1, eixox2 , eixoz1 ],dtype=float)
         y= np.array([eixoy1 , eixoy2 , eixoz2],dtype=float)
         z= np.array([eixox3, eixoy3, eixoz3],dtype=float)
+        
+        #make graph
         ax = plt.figure().add_subplot(projection='3d')
         ax.plot(x,y,z)
+        # save the graph as a png
         plt.savefig('static\images\graph.png')
-        plt.show()
-    return render_template("index.html")
 
-@app.route("/graph")
-def graph():
-    return render_template("graph.html")
+        # if POST is requested render the page and return the graph directory as a variable
+        return render_template("index.html", img = '/static/images/graph.png')
+    else:
+        # render the page if no POST is requested
+        return render_template("index.html")
 
 if __name__=='__main__':
     app.run()
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Learning Flask 1-  Numpy and Graphs
- Learning Flask and putting it on pratice in a personal exercise  to create a site with an interactable  graph.
+# Learning Flask 
+1 - Numpy and Graphs
+2 - Forms and Showing results
