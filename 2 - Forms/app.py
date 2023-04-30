7d75753f1071abf0d9e0a3b1fb7530359296c24e
MarkVN2 <markosvinicius.nunes@gmail.com>
2023-04-30 19:55:30 -0300

    Added minor error treatment

--- 2 - Forms/app.py
+++ 2 - Forms/app.py
@@ -5,15 +5,17 @@ app = Flask(__name__)
 @app.route("/", methods =["GET", "POST"])
 def index():
     if request.method == "POST":
-        # grab A and B from the page
-        a = int(request.form.get("a"))
-        b = int(request.form.get("b"))
-        #Make C equal to the result of the equation
-        c = a + b  
-        # With post we render the page and also send variable C to the page
-        return render_template("index.html", c = c)
+        try:
+            # grab A and B from the page
+            a = int(request.form.get("a"))
+            b = int(request.form.get("b"))
+            #Make C equal to the result of the equation
+            c = a + b  
+            # With post we render the page and also send variable C to the page
+            return render_template("index.html", c = c)
+        except  ValueError:
+            return render_template("index.html")
     else:
-        # if no POST method is requested render only the page.
         return render_template("index.html")
 
 if __name__=='__main__':
