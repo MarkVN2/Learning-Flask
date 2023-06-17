4b18f6ef632edbcf9f316d7d1fece540db10b397
MarkVN2 <markosvinicius.nunes@gmail.com>
2023-06-17 19:45:26 -0300

    Finished redesign in 'Numpy and Graphs'
    Page has a better look

--- 1 -Numpy and Graphs/app.py
+++ 1 -Numpy and Graphs/app.py
@@ -6,30 +6,24 @@ app = Flask(__name__)
 
 @app.route("/", methods =["GET", "POST"])
 def index():
-    
-        # render the page if no POST is requested
-        return render_template("index.html")
-
-@app.route("/graph" , methods =["GET", "POST"])
-def graph():
     if request.method == "POST":
         #This could be done much more efficiently
         
-        eixox1 = request.form.get("eixox1")
-        eixoy1 = request.form.get("eixoy1")
-        eixoz1 = request.form.get("eixoz1")
+        x1 = request.form.get("x1")
+        y1 = request.form.get("y1")
+        z1 = request.form.get("z1")
 
-        eixox2 = request.form.get("eixox2")
-        eixoy2 = request.form.get("eixoy2")
-        eixoz2 = request.form.get("eixoz2")
+        x2 = request.form.get("x2")
+        y2 = request.form.get("y2")
+        z2 = request.form.get("z2")
 
-        eixox3 = request.form.get("eixox3")
-        eixoy3 = request.form.get("eixoy3")
-        eixoz3 = request.form.get("eixoz3")
+        x3 = request.form.get("x3")
+        y3 = request.form.get("y3")
+        z3 = request.form.get("z3")
 
-        x= np.array([eixox1, eixox2 , eixoz1 ],dtype=float)
-        y= np.array([eixoy1 , eixoy2 , eixoz2],dtype=float)
-        z= np.array([eixox3, eixoy3, eixoz3],dtype=float)
+        x= np.array([x1, x2 , z1 ],dtype=float)
+        y= np.array([y1 , y2 , z2],dtype=float)
+        z= np.array([x3, y3, z3],dtype=float)
         
         #make graph
         ax = plt.figure().add_subplot(projection='3d')
@@ -37,10 +31,16 @@ def graph():
         # save the graph as a png
         plt.savefig('static\images\graph.png')
 
-        # if POST is requested render the page and return the graph directory as a variable
-        return render_template("graph.html", img = '/static/images/graph.png')
+        return render_template("index.html")
     else:
+        return render_template("index.html")
+
+@app.route("/graph")
+def graph():
         return render_template("graph.html")
-        
+
+@app.route("/test")
+def test():
+    return "test"
 if __name__=='__main__':
     app.run()
